d9f94fe395a5bd845add20b9ca0f91e6eaa49b55
Caleb Briancesco <calebbriancesco@hotmail.com>
2015-08-23 21:52:18 -0600

    Adding file for db creation and products population

---
+++ data/vending_machine_products.js
@@ -0,0 +1,13 @@
+db.products.insert(
+   [
+    {"name" : "Chocolate Bar", "price" : '3.00', "quantity" : 4, "image": 'img/chocolate.png'},
+	{"name" : "Muffin", "price" : '4.75', "quantity" : 6, "image": 'img/muffin.png'},
+	{"name" : "Potato Chips", "price" : '2.50', "quantity" : 9, "image": 'img/chips.png'},
+	{"name" : "Bubble Gum", "price" : '0.50', "quantity" : 10, "image": 'img/gum.png'},
+	{"name" : "Donut", "price" : '3.25', "quantity" : 4, "image": 'img/donut.png'},
+	{"name" : "Cookie", "price" : '1.50', "quantity" : 4, "image": 'img/cookie.png'},
+	{"name" : "Coke", "price" : '2.00', "quantity" : 9, "image": 'img/coke.png'},
+	{"name" : "Pepsi", "price" : '2.00', "quantity" : 4, "image": 'img/pepsi.png'},
+	{"name" : "Water", "price" : '2.50', "quantity" : 5, "image": 'img/water.png'}
+   ]
+)
